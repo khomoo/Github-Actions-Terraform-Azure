0a8a597bfd531814e0eb1a3c1ad8f5d6a779ad7d
khomoo <87760257+khomoo@users.noreply.github.com>
2025-08-15 07:56:51 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,13 +9,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  provider "azurerm" {
-  features {}
-
   subscription_id = "f8061c23-bc7b-4524-aed7-a5e97d5186f8"
   client_id       = "e361d39d-2bdc-456e-84cb-b9fa5372bb9e"
   client_secret   = "gVs8Q~-UMnkfvSmT5YlrvV9SXE7_8VhoseIyYatT"
   tenant_id       = "f7c21cad-4eb5-4b77-b3ed-a604304c788f"
 }
 
-}
